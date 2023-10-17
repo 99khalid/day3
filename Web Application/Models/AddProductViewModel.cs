49cb8e88640a843c997f92ab0d1f6eea77815e19
99khalid <khalidhassan912999@gmail.com>
2023-10-17 12:52:21 +0300

    task day 3

---
+++ Web Application/Models/AddProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Web_Application.Models
+{
+    public class AddProductViewModel
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
